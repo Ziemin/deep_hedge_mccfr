f452a5ed296b21e02052c62c4e97d5eb32662975
Marcin Ziemiński <ziemin@apache.org>
2020-06-08 17:14:21 +0200

    Implemented print utilities

--- app/inspect_game.cpp
+++ app/inspect_game.cpp
@@ -1,22 +1,88 @@
 #include <absl/flags/flag.h>
 #include <absl/flags/parse.h>
 #include <absl/random/uniform_int_distribution.h>
+#include <dmc/format.hpp>
 #include <fmt/core.h>
 #include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#include <open_spiel/game_parameters.h>
 #include <open_spiel/spiel.h>
+#include <random>
 #include <string>
+#include <unistd.h>
 #include <vector>
 
-ABSL_FLAG(std::string, game, "tic_tac_toe", "The name of the game to play.");
+ABSL_FLAG(std::string, game, "", "The name of the game to play.");
 ABSL_FLAG(int, players, 0, "How many players in this game, 0 for default.");
 ABSL_FLAG(int, seed, 0, "Seed for the random number generator. 0 for auto.");
+ABSL_FLAG(bool, playthrough, false, "Generate random playthrough.");
+ABSL_FLAG(bool, print_legal_actions, false, "Print all legal actions");
+ABSL_FLAG(bool, print_string_reprs, false,
+          "Print string state representations");
+ABSL_FLAG(bool, print_tensors, false, "Print tensor state representatins");
 
+void generate_playthrough(const open_spiel::Game &game, int seed,
+                          bool print_legal_actions, bool print_strings,
+                          bool print_tensors) {
+  std::mt19937 rng(seed);
 
-int main(int argc, char** argv) {
+  std::unique_ptr<open_spiel::State> state = game.NewInitialState();
+  std::string state_format_str =
+      fmt::format("\n--- State: --------------------------\n{{:{}{}{}}}\n",
+                  print_legal_actions ? "a" : "",
+                  print_strings ? "s" : "",
+                  print_tensors ? "t" : "");
+
+  while (!state->IsTerminal()) {
+    fmt::print(state_format_str, *state);
+
+    if (state->IsChanceNode()) {
+
+      open_spiel::ActionsAndProbs outcomes = state->ChanceOutcomes();
+      open_spiel::Action action = open_spiel::SampleAction(outcomes, rng).first;
+      fmt::print("Sampled Outcome: {}\n",
+                 state->ActionToString(open_spiel::kChancePlayerId, action));
+      state->ApplyAction(action);
+
+    } else if (state->IsSimultaneousNode()) {
+
+      std::vector<open_spiel::Action> joint_action;
+      for (auto player = open_spiel::Player{0}; player < game.NumPlayers();
+           ++player) {
+        std::vector<open_spiel::Action> actions = state->LegalActions(player);
+        absl::uniform_int_distribution<> dis(0, actions.size() - 1);
+        open_spiel::Action action = actions[dis(rng)];
+        joint_action.push_back(action);
+        fmt::format("Player {} chose {}\n", player,
+                    state->ActionToString(player, action));
+      }
+      state->ApplyActions(joint_action);
+
+    } else {
+      int player = state->CurrentPlayer();
+      std::vector<open_spiel::Action> actions = state->LegalActions();
+      absl::uniform_int_distribution<> dis(0, actions.size() - 1);
+      auto action = actions[dis(rng)];
+      fmt::print("Sampled Action: {}\n", state->ActionToString(player, action));
+      state->ApplyAction(action);
+    }
+  }
+  fmt::print("Final State:\n{}\n", *state);
+}
+
+int main(int argc, char **argv) {
   absl::ParseCommandLine(argc, argv);
   const std::string game_name = absl::GetFlag(FLAGS_game);
   const int players = absl::GetFlag(FLAGS_players);
   const int seed = absl::GetFlag(FLAGS_seed);
+  const bool gen_playthrough = absl::GetFlag(FLAGS_playthrough);
+
+  if (game_name.empty()) {
+    fmt::print("Please, provide a game name.\nAvailable games:\n- {}\n",
+               fmt::join(open_spiel::RegisteredGames(), "\n- "));
+    return -1;
+  }
 
   fmt::print("Creating a game..\n");
   open_spiel::GameParameters params;
@@ -28,7 +94,15 @@ int main(int argc, char** argv) {
     fmt::print(stderr, "Problem loading game {}, exiting...", game_name);
     return -1;
   }
+  fmt::print("{}\n", *game);
+
+  if (gen_playthrough) {
+    fmt::print("Generating random playthrough...\n");
+    generate_playthrough(*game, seed == 0 ? time(0) : seed,
+                         absl::GetFlag(FLAGS_print_legal_actions),
+                         absl::GetFlag(FLAGS_print_string_reprs),
+                         absl::GetFlag(FLAGS_print_tensors));
+  }
 
   return 0;
 }
-
---
+++ include/dmc/format.hpp
@@ -0,0 +1,212 @@
+#pragma once
+#include <fmt/format.h>
+#include <open_spiel/game_parameters.h>
+#include <open_spiel/spiel.h>
+#include <range/v3/all.hpp>
+
+namespace fmt {
+template <>
+struct formatter<open_spiel::GameType::RewardModel>
+    : formatter<std::string_view> {
+  template <typename FormatContext>
+  auto format(open_spiel::GameType::RewardModel rm, FormatContext &ctx) {
+    using RewardModel = open_spiel::GameType::RewardModel;
+    std::string_view name =
+        rm == RewardModel::kRewards ? "RL-Style" : "Game-Style";
+    return formatter<std::string_view>::format(name, ctx);
+  }
+};
+
+template <>
+struct formatter<open_spiel::TensorLayout> : formatter<std::string_view> {
+  template <typename FormatContext>
+  auto format(open_spiel::TensorLayout tl, FormatContext &ctx) {
+    std::string_view name =
+        tl == open_spiel::TensorLayout::kCHW ? "CHW" : "HWC";
+    return formatter<std::string_view>::format(name, ctx);
+  }
+};
+
+template <>
+struct formatter<open_spiel::GameParameter::Type>
+    : formatter<std::string_view> {
+  template <typename FormatContext>
+  auto format(open_spiel::GameParameter::Type type, FormatContext &ctx) {
+    std::string_view name = "unknown";
+    switch (type) {
+    case open_spiel::GameParameter::Type::kUnset:
+      name = "unset";
+      break;
+    case open_spiel::GameParameter::Type::kInt:
+      name = "int";
+      break;
+    case open_spiel::GameParameter::Type::kDouble:
+      name = "double";
+      break;
+    case open_spiel::GameParameter::Type::kString:
+      name = "string";
+      break;
+    case open_spiel::GameParameter::Type::kBool:
+      name = "bool";
+      break;
+    case open_spiel::GameParameter::Type::kGame:
+      name = "Game";
+      break;
+    };
+    return formatter<std::string_view>::format(name, ctx);
+  }
+};
+
+template <> struct formatter<open_spiel::Game> {
+
+  constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+  template <typename FormatContext>
+  auto format(const open_spiel::Game &game, FormatContext &ctx) {
+    auto it = ctx.out();
+    auto game_type = game.GetType();
+    it = format_to(it, "Game: {} ({})\n", game_type.long_name,
+                   game_type.short_name);
+    it = format_to(it, "* Dynamics: {}\n", game_type.dynamics);
+    it = format_to(it, "* Chance Mode: {}\n", game_type.chance_mode);
+    it = format_to(it, "* Information: {}\n", game_type.information);
+    it = format_to(it, "* Utility: {}\n", game_type.utility);
+    it = format_to(it, "* Utility Range: [{}, {}]\n", game.MinUtility(),
+                   game.MaxUtility());
+    if (game_type.utility == open_spiel::GameType::Utility::kConstantSum) {
+      it = format_to(it, "\t- Utility Sum: {}\n", game.UtilitySum());
+    }
+    it = format_to(it, "* Reward Model: {}\n", game_type.reward_model);
+    it = format_to(it, "* Num Distinct Actions: {}\n",
+                   game.NumDistinctActions());
+    it = format_to(it, "* Policy Tensor Shape: {}\n", game.PolicyTensorShape());
+    it = format_to(it, "* Max Chance Outcomes: {}\n", game.MaxChanceOutcomes());
+    it = format_to(it, "* Num Players: {}\n", game.NumPlayers());
+    it = format_to(it, "* Maximum Game Lenth (over player actions): {}\n",
+                   game.MaxGameLength());
+
+    it = format_to(it, "* Provides Information State String: {}\n",
+                   game_type.provides_information_state_string);
+    it = format_to(it, "* Provides Information State Tensor: {}\n",
+                   game_type.provides_information_state_tensor);
+    if (game_type.provides_information_state_tensor) {
+      it = format_to(it, "\t- Tensor Shape: {}\n",
+                     game.InformationStateTensorShape());
+      it = format_to(it, "\t- Tensor Layout: {}\n",
+                     game.InformationStateTensorLayout());
+    }
+    it = format_to(it, "* Provides Observation String: {}\n",
+                   game_type.provides_observation_string);
+    it = format_to(it, "* Provides Observation Tensor: {}\n",
+                   game_type.provides_observation_string);
+    if (game_type.provides_observation_tensor) {
+      it = format_to(it, "\t- Tensor Shape: {}\n",
+                     game.ObservationTensorShape());
+      it = format_to(it, "\t- Tensor Layout: {}\n",
+                     game.ObservationTensorLayout());
+    }
+    it = format_to(it, "* Game Params:\n");
+    for (const auto &[name, param] : game_type.parameter_specification) {
+      it = format_to(it, "\t- '{}': {} {}\n", name, param.type(),
+                     param.is_mandatory() ? "(required)" : "");
+    }
+    return it;
+  }
+};
+
+template <> struct formatter<open_spiel::State> {
+  bool print_legal_actions = false;
+  bool print_string_repr = false;
+  bool print_tensor_repr = false;
+
+  constexpr auto parse(format_parse_context &ctx) {
+    auto end = ctx.end();
+    auto it = ctx.begin();
+    while (it != end && *it != '}') {
+      if (*it == 'a')
+        print_legal_actions = true;
+      else if (*it == 's')
+        print_string_repr = true;
+      else if (*it == 't')
+        print_tensor_repr = true;
+      else
+        throw fmt::format_error(
+            fmt::format("Unrecognized format char: {}", *it));
+      ++it;
+    }
+    if (it != end && *it != '}')
+      throw fmt::format_error("Invalid format");
+    return it;
+  }
+
+  template <typename FormatContext>
+  auto format(const open_spiel::State &state, FormatContext &ctx) {
+    auto it = ctx.out();
+    auto game = state.GetGame();
+    auto game_type = game->GetType();
+
+    if (state.IsTerminal()) {
+      it = format_to(it, "Type: Terminal");
+      return it;
+    }
+
+    auto player = state.CurrentPlayer();
+    if (state.IsChanceNode()) {
+      it = format_to(it, "Type: Chance\n");
+    } else if (state.IsSimultaneousNode()) {
+      it = format_to(it, "Type: Simultaneous\n");
+
+      for (auto player = open_spiel::Player{0}; player < game->NumPlayers(); player++) {
+        if (print_string_repr or print_tensor_repr)
+          it = format_to(it, "Player {} perspective:\n", player);
+        if (print_string_repr) {
+          if (game_type.provides_information_state_string)
+            it = format_to(it, "Info State String:\n{}\n",
+                           player, state.InformationStateString(player));
+          if (game_type.provides_observation_string)
+            it = format_to(it, "Obervation String:\n{}\n",
+                           player, state.ObservationString(player));
+        }
+        if (print_tensor_repr) {
+          if (game_type.provides_information_state_tensor)
+            it = format_to(it, "Info State Tensor:\n{}\n",
+                           state.InformationStateTensor(player));
+          if (game_type.provides_observation_tensor)
+            it = format_to(it, "Observation Tensor:\n{}\n",
+                           state.ObservationTensor(player));
+        }
+      }
+
+    } else {
+      it = format_to(it, "Type: Decision\n");
+      it = format_to(it, "Current Player: {}\n", player);
+
+      if (print_string_repr) {
+        if (game_type.provides_information_state_string)
+          it = format_to(it, "Info State String:\n{}\n", state.InformationStateString(player));
+        if (game_type.provides_observation_string)
+          it = format_to(it, "Obervation String:\n{}\n", state.ObservationString(player));
+      }
+      if (print_tensor_repr) {
+        if (game_type.provides_information_state_tensor)
+          it = format_to(it, "Info State Tensor:\n{}\n", state.InformationStateTensor(player));
+        if (game_type.provides_observation_tensor)
+          it = format_to(it, "Observation Tensor:\n{}\n",
+                         state.ObservationTensor(player));
+      }
+
+      if (print_legal_actions) {
+        it = format_to(it, "Legal Actions:\n");
+        auto actions = state.LegalActions(player);
+        auto action_strs_rng = actions | ranges::views::transform([&state](open_spiel::Action act) {
+              return state.ActionToString(state.CurrentPlayer(), act);
+            });
+        it = format_to(it, "{}\n", fmt::join(action_strs_rng, ", "));
+      }
+
+    }
+    return it;
+  }
+};
+
+} // namespace fmt
